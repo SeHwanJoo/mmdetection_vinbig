1915b7b8a08c429171921cb23a6f2a824a4c47ea
famer9716 <famer3625@gmail.com>
2021-03-30 16:58:38 +0900

    add faster_resnext_pretrained.py

--- configs/vinbig/fasterRCNN_fin.py
+++ configs/vinbig/fasterRCNN_fin.py
@@ -224,11 +224,12 @@ data = dict(
                 ])
         ]))
 lr_config = dict(
-    policy='cyclic',
-    target_ratio=(10, 0.1),
-    cyclic_times=4,
-    step_ratio_up=0.05)
-optimizer = dict(type='SGD', lr=0.0004, momentum=0.9, weight_decay=0.0001)
+    policy='CosineAnnealing',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=0.1,
+    min_lr_ratio=1e-05)
+optimizer = dict(type='SGD', lr=0.0005, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 runner = dict(type='EpochBasedRunner', max_epochs=40)
 checkpoint_config = dict(interval=1)
